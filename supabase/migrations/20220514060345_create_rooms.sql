1b46c885e6d16b2851598c7529f8295cfbd84273
HIboyGigiggty <104076264+HIboyGigiggty@users.noreply.github.com>
2022-05-14 20:55:47 +0800

    db: remove the useless funtion get_room_by_id

--- supabase/migrations/20220514060345_create_rooms.sql
+++ supabase/migrations/20220514060345_create_rooms.sql
@@ -3,31 +3,6 @@
 -- and may require manual changes to the script to ensure changes are applied in the correct order.
 -- Please report an issue for any failure with the reproduction steps.
 
-CREATE OR REPLACE FUNCTION public.get_room_by_id(
-	room_id uuid)
-    RETURNS SETOF rooms 
-    LANGUAGE 'sql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-    ROWS 1000
-
-AS $BODY$
-select id, owner, name, created_at from rooms where id = room_id;
-$BODY$;
-
-ALTER FUNCTION public.get_room_by_id(uuid)
-    OWNER TO postgres;
-
-GRANT EXECUTE ON FUNCTION public.get_room_by_id(uuid) TO PUBLIC;
-
-GRANT EXECUTE ON FUNCTION public.get_room_by_id(uuid) TO anon;
-
-GRANT EXECUTE ON FUNCTION public.get_room_by_id(uuid) TO authenticated;
-
-GRANT EXECUTE ON FUNCTION public.get_room_by_id(uuid) TO postgres;
-
-GRANT EXECUTE ON FUNCTION public.get_room_by_id(uuid) TO service_role;
-
 CREATE TABLE IF NOT EXISTS public.rooms
 (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
@@ -56,6 +31,7 @@ GRANT ALL ON TABLE public.rooms TO authenticated;
 GRANT ALL ON TABLE public.rooms TO postgres;
 
 GRANT ALL ON TABLE public.rooms TO service_role;
+
 CREATE POLICY "users can see and operate rooms they own"
     ON public.rooms
     AS PERMISSIVE
