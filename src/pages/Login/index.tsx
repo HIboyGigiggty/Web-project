a4f035ac68854069dc95f0096428811c15d25aed
thisLight <l1589002388@gmail.com>
2022-05-15 14:51:07 +0800

    pages/Login: fix not jumped to index after back from supabase

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,25 @@
-import { Component, Show } from "solid-js"
-import { createSupabaseAuth } from "solid-supabase"
-import { Navigate } from "solid-app-router";
+import { Component, onMount, Show } from "solid-js"
+import { createOnAuthStateChange, createSupabaseAuth } from "solid-supabase"
+import { Navigate, useNavigate } from "solid-app-router";
 
 const Login: Component = () => {
     const auth = createSupabaseAuth();
+    const navigate = useNavigate();
     let signInWithGithub = async () => {
         const { user, session, error } = await auth.signIn({
             provider: 'github',
         });
     };
+    createOnAuthStateChange((_, session) => {
+        // Check again to force jump back.
+        // The Show's fallback could not be rendered when jumping back from supabase.
+        // TODO: allow jumping back to specific address.
+        if (session) {
+            if (session.user) {
+                navigate("/");
+            }
+        }
+    });
     return (<>
         <h1>Sign in with...</h1>
         <Show when={!auth.user()} fallback={<Navigate href="/" />}>
