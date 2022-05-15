eb785fab418add607748cdb93ffa12237b7903ea
HIboyGigiggty <104076264+HIboyGigiggty@users.noreply.github.com>
2022-05-15 12:15:43 +0800

    start

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,13 +1,14 @@
 import Button from "@suid/material/Button";
 import TextField from "@suid/material/TextField";
 import { Navigate, useNavigate } from "solid-app-router";
-import { Component, createResource, Show, For } from "solid-js";
+import { Component, createResource, Show, For, createSignal } from "solid-js";
 import { createSupabase, createSupabaseAuth } from "solid-supabase";
 
 const Index: Component = () => {
     const auth = createSupabaseAuth();
     const supabase = createSupabase();
     const navigate = useNavigate();
+    let [roomName, setRoomName] = createSignal<string>("")
 
     let getAllRooms = async () => {
         const user = auth.user();
@@ -21,13 +22,31 @@ const Index: Component = () => {
             navigate("/login")
         }
     }
-    let [rooms] = createResource(getAllRooms, {
+    let [rooms, {refetch}] = createResource(getAllRooms, {
         initialValue: []
     });
+    const creating = async () => {
+        console.log("creating");
+        let user = auth.user();
+        if (user) {
+            const {data, error}= await supabase.from("rooms").insert({
+                name: roomName(),
+                owner: user.id,
+            });
+            if (error){
+                throw error;
+            }
+            setRoomName("");
+            await refetch();
+        } else {
+            navigate("/login");
+        }
+        
+    }
     return <Show when={auth.user()} fallback={<Navigate href="/login" />}>
-        <Button onClick={() => navigate("/user")}>User Infomation</Button><br></br>
-        <TextField label="RoomName" helperText="Plz input your Room name"></TextField>
-        <Button >Create Room</Button>
+        <Button className="button1" onClick={() => navigate("/user")}>User Infomation</Button><br></br>
+        <TextField className="testfield1" label="RoomName" helperText="Plz input your Room name" value={roomName()} onChange={(_, val) => setRoomName(val)}></TextField>
+        <Button onClick={creating} >Create Room</Button>
         <Show when={!rooms.loading} fallback={<p>Loading rooms</p>}>
             <For each={rooms()} fallback={<p>No rooms here.</p>}>
                 {
@@ -37,6 +56,19 @@ const Index: Component = () => {
                 }
             </For>
         </Show>
+        <style jsx>
+            {`
+            .button1{
+                background-color:blue;
+                color: white;
+                border: none;
+                font-size: 20px;
+                }
+            .textfield1{
+                border: none;
+                }
+            `}
+        </style>
     </Show>
 }
 
