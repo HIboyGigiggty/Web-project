f58edc1b99b8452ffc74c0c1b9ba3ebfb612dd43
thisLight <l1589002388@gmail.com>
2022-05-28 11:34:25 +0800

    pages/Room: join the room on mounting

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,10 +1,46 @@
+import CircularProgress from "@suid/material/CircularProgress";
 import { useParams } from "solid-app-router";
-import { Component } from "solid-js";
+import { Component, Match, Switch, createSignal, onMount } from "solid-js";
+import { useBroadClient } from "../../helpers/BroadClient/solid";
+
+enum RoomStatus {
+    "Unknown",
+    "Found",
+    "Joint",
+    "NotFound",
+}
 
 const Room: Component = () => {
     const params = useParams();
+    const broadCli = useBroadClient();
+    const [status, setStatus] = createSignal<RoomStatus>(RoomStatus.Unknown);
+
+    onMount(async () => {
+        const room = await broadCli.findRoomById(params.id);
+        if (room) {
+            setStatus(RoomStatus.Found);
+            if (!await broadCli.isJointRoomById(room.id)) {
+                await broadCli.joinRoomById(room.id);
+            }
+            setStatus(RoomStatus.Joint);
+        } else {
+            setStatus(RoomStatus.NotFound);
+        }
+    });
+    
     return <>
-        <span>Room for {params.id}</span>
+        <span>Room for {params.id}</span><br />
+        <Switch fallback={<span>Unknown Problem.</span>}>
+            <Match when={status() === RoomStatus.Unknown || status() === RoomStatus.Found}>
+                <CircularProgress color="secondary" />Please wait...
+            </Match>
+            <Match when={status() === RoomStatus.Joint}>
+                <span>You join this room.</span>
+            </Match>
+            <Match when={status() === RoomStatus.NotFound}>
+                <span>Not Found.</span>
+            </Match>
+        </Switch>
     </>;
 };
 
