9b4cd31719491c67bed0450993b16857af134df9
Liei <ww15977627289@gmail.com>
2022-06-02 08:45:53 +0800

    batterInterface

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -11,12 +11,18 @@ import AddIcon from "@suid/icons-material/Add";
 import Card from "@suid/material/Card";
 import List from "@suid/material/List";
 import Avatar from "@suid/material/Avatar";
-
+{/*-----------------------------------------*/}
+import CardActions from "@suid/material/CardActions";
+import Modal from "@suid/material/Modal";
+// import useTheme from "@suid/material/styles/useTheme";
+import ListItem from "@suid/material/ListItem";
+{/*-----------------------------------------*/}
 import { Navigate, useNavigate } from "solid-app-router";
 import { Component, For, Show, createResource, createSignal } from "solid-js";
 import { createSupabase, createSupabaseAuth } from "solid-supabase";
 import BroadClient from "../../helpers/BroadClient";
 import CardContent from "@suid/material/CardContent";
+import Popover from "@suid/material/Popover";
 
 
 
@@ -26,6 +32,65 @@ const useBroadClient = () => {
     return new BroadClient(supabase);
 };
 
+const UserAvatar :Component=() =>{//头像组件
+    const auth = createSupabaseAuth();
+    const user = auth.user();
+    let buttomRef : HTMLButtonElement;
+    const [datailPopoverOpen,setdatailPopoverOpen] = createSignal<boolean>(false);
+    return(
+        <>
+        <Button
+        variant="contained"
+        size="medium"
+        onClick={() =>setdatailPopoverOpen(true)}
+        //@ts-expect-error
+        ref={buttomRef}
+        disableElevation
+        disableRipple
+        //@ts-expect-error
+        sx={{
+            padding:0,
+            miniwidth:"30px",
+        }}
+        >
+            <Avatar/>
+        </Button>
+        <Popover
+            anchorOrigin={{
+                vertical:"bottom",
+                horizontal:"left"
+            }}
+            open={datailPopoverOpen()}
+            //@ts-expect-error
+            onClose={setdatailPopoverOpen(false)}
+            // @ts-expect-error
+            anchorEl={buttomRef}
+        >
+            <Card>
+                <CardContent sx={{
+                    padding:0,
+                }}>
+                    <Typography sx={{
+                        padding:0
+                    }}>
+                        <List sx={{
+                            padding:0,
+                        }}>
+                            <ListItem sx={{
+                                padding:0
+                            }}>
+                                用户ID：<br></br>
+                                {user?.id}
+                            </ListItem>
+                        </List>
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Popover>
+        </>
+    )
+}
+
 const Index: Component = () => {
     const auth = createSupabaseAuth();
     const navigate = useNavigate();
@@ -33,7 +98,7 @@ const Index: Component = () => {
 
     const [roomName, setRoomName] = createSignal<string>("");
 
-    const getAllRooms = async () => {//获取所以房间
+    const getAllRooms = async () => {
         const user = auth.user();
         if (user) {
             return await broadCli.getAllRooms();
@@ -44,7 +109,7 @@ const Index: Component = () => {
     const [rooms, {refetch}] = createResource(getAllRooms, {
         initialValue: []
     });
-    const creating = async () => {//创建房间方法
+    const creating = async () => {
         console.log("creating");
         const user = auth.user();
         if (user) {
@@ -56,60 +121,102 @@ const Index: Component = () => {
         }
     };
 
-    const AlterAnThings =async() =>{
-        window.alert("开发中！");
-    };
+
+    {/*-----------------------------------------*/}
+    const [open, setOpen] = createSignal(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    {/*-----------------------------------------*/}
 
     return <Show when={auth.user()} fallback={<Navigate href="/login" />}>
+        {/*---------------------App bar--------------------*/}
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
+                    
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         画板
                     </Typography>
-                    <Avatar>
-                        <Button onClick={AlterAnThings}>
-                            H
-                        </Button>
-                    </Avatar>
+                    <UserAvatar/>
                 </Toolbar>
             </AppBar>
         </Box>
+        {/*-------------------App bar----------------------*/}
 
+        {/*---------------------FAB--------------------*/}
         <Box sx={{
-            right:40,
-            position:"absolute",
-            bottom:50,
+            right: 40,
+            position: "absolute",
+            bottom: 50,
+            padding: 0,
+            paddingTop: 0,
         }}>
-            <Fab color="primary" aria-label="add">
-                <AddIcon onClick={AlterAnThings}/>
-            </Fab>
+            <div>
+                <Fab color="primary" aria-label="add" onClick={handleOpen}>
+                    <AddIcon/>
+                </Fab>
+                <Modal
+                    open={open()}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Card sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        width: 350,
+                        border: "0px solid #000",
+                        p: 4,
+                        padding: "15px",
+                        paddingTop: "16px",
+                        paddingBottom: 0,
+                    }}>
+                        <CardContent sx={{
+                            padding: 0,
+                        }}>
+                            <List disablePadding>
+                                <ListItem disablePadding>
+                                    <TextField sx={{width: "100%"}} label="RoomName" variant="standard"
+                                               onChange={(_, val) => setRoomName(val)}
+                                               helperText="Please input your Room name" value={roomName()}> </TextField>
+                                </ListItem>
+                            </List>
+                        </CardContent>
+                        <CardActions>
+                            <Box sx={{
+                                width: "100%",
+                                padding: "5px",
+                            }}/>
+                            <Button sx={{
+                                width: "50%",
+                                padding: "5px",
+                            }} onClick={async () => {
+                                setOpen(false);
+                                // await AlterAnThings();
+                                await creating()
+                            }}>create&nbspRoom</Button>
+                        </CardActions>
+                    </Card>
+                </Modal>
+            </div>
         </Box>
+                    {/*--------------------FAB---------------------*/}
 
+                    {/*--------------------List---------------------*/}
         <Box sx={{ml: "50%", transform: "translate(-50%, 50%)", width: "fit-content", padding:0}} >
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    <Typography>
-                        <TextField variant="standard" placeholder="房间ID"></TextField>
-                    </Typography>
+            <Card sx={{minWidth: 275}}>
+                <CardContent sx={{padding:0}} style="padding: 0;">
+                    <TextField sx={{width: "100%"}}  placeholder="房间ID"></TextField>
                 </CardContent>
             </Card>
         </Box>
 
-        <Box sx={{ml: "50%", transform: "translate(-50%, 50%)", width: "fit-content", padding:0}}>
-            <Card sx={{padding:0,minWidth:275}}>
-                <CardContent>                
-                    <Typography>
-                        <List>
+        <Box sx={{ml: "50%", transform: "translate(-50%, 35%)", width: "fit-content", padding:0}}>
+            <Card sx={{minWidth:275}}>
+                <CardContent sx={{padding:0}} style="padding: 0;">                
+                        <List sx={{padding:0}}>
                             <For each={rooms()} fallback={<List>No rooms here.</List>}>
                                 {
                                     (item) => {
@@ -117,18 +224,12 @@ const Index: Component = () => {
                                     }
                                 }
                             </For>
-                        </List>
-                    </Typography>                    
+                        </List>                   
                 </CardContent>
             </Card>
         </Box>
+                    {/*------------------List-----------------------*/}
 
-        <TextField class="testfield1" label="RoomName" helperText="Plz input your Room name" value={roomName()} onChange={(_, val) => setRoomName(val)}></TextField>
-
-        <Button onClick={creating} >Create Room</Button><br></br>
-        
-        <Show when={!rooms.loading} fallback={<p>Loading rooms</p>}>
-        </Show>
     </Show>;
 };
 
