23a79cd2e7a5b56efaaefec689ca50614bb6c36d
thisLight <l1589002388@gmail.com>
2022-06-04 17:38:24 +0800

    fix: SupabaseClient.channel() return RealtimeSubscription not RealtimeChannel

--- src/supabase_client.ts
+++ src/supabase_client.ts
@@ -1,6 +1,12 @@
 import supabase_conf from "./configs/supabase_conf";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase_client = createClient(supabase_conf.url, supabase_conf.key);
+const supabase_client = createClient(supabase_conf.url, supabase_conf.key, {
+    realtime: {
+        params: {
+            vsndate: "1.0.0", // required to use realtime (but it's not specified in supabase-js document...:/)
+        }
+    }
+});
 
 export default supabase_client;
