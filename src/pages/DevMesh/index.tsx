eb6959c2b80dd62ca63d6de306dd458aa63223d3
thisLight <l1589002388@gmail.com>
2022-06-05 16:57:18 +0800

    pages/DevMesh: add network related infomation

--- src/pages/DevMesh/index.tsx
+++ src/pages/DevMesh/index.tsx
@@ -1,16 +1,21 @@
 import Button from "@suid/material/Button";
 import TextField from "@suid/material/TextField";
-import { Component, createResource, createSignal, For, Show } from "solid-js";
+import { Component, createResource, createSignal, For, onCleanup, Show } from "solid-js";
 import { useBroadClient } from "../../helpers/BroadClient/solid";
 import { createSupabaseAuth } from "solid-supabase";
 import { Participant } from "../../helpers/BroadClient";
 import { validate as uuidValidate } from "uuid";
+import { Peer, Router } from "../../helpers/mesh";
+import { SupabaseDatachannel } from "../../helpers/mesh/supabase";
 
 const DevMesh: Component = () => {
     const [roomName, setRoomName] = createSignal<string>("");
     const [roomId, setRoomId] = createSignal<string>("");
+    const [peers, setPeers] = createSignal<Peer[]>([]);
     const broadClient = useBroadClient();
     const auth = createSupabaseAuth();
+
+    const [routerG, setRouterG] = createSignal<Router>();
 
     const [participants, participantsCtl] = createResource<Participant[]>(() => {
         if (uuidValidate(roomId())) {
@@ -24,6 +29,16 @@ const DevMesh: Component = () => {
             await broadClient.joinRoomById(roomId);
         }
         participantsCtl.refetch();
+        const alterChan = SupabaseDatachannel.ofRoom(broadClient.supabase, roomId, broadClient.getUserDeviceId());
+        const router = new Router(broadClient.getUserDeviceId(), alterChan);
+        router.bus.on("addpeer", () => {
+            setPeers(router.peers);
+        });
+        router.bus.on("removepeer", () => {
+            setPeers(router.peers);
+        });
+        await router.broadcastPeerList(roomId);
+        setRouterG(router);
     };
 
     const createRoom = async () => {
@@ -44,15 +59,37 @@ const DevMesh: Component = () => {
         }
     };
 
+    onCleanup(async () => {
+        const routerg = routerG();
+        if (routerg) {
+            await routerg.stop();
+            setRouterG();
+        }
+    });
+
+    const getRouterId = () => {
+        const routerg = routerG();
+        if (routerg) {
+            return routerg.userDeviceId;
+        }
+    };
+
     return <div>
         <p>User: {JSON.stringify(auth.user(), undefined, 2)}</p>
         <TextField variant="standard" value={roomName()} label="Room Name" onChange={(ev) => setRoomName(ev.target.value)} /><Button onClick={createRoom}>Create New Room</Button><br />
         <TextField variant="standard" value={roomId()} label="Room ID" onChange={(ev) => setRoomId(ev.target.value)} /><Button onClick={enterRoom}>Enter Room</Button><br />
+        <p>User-Device Id: {getRouterId() || "unknown"}</p>
         <div>
             <Show when={participants.loading}><p>Loading participants...</p></Show>
             <p>Participants:</p>
             <ul>
                 <For each={participants()}>{(p) => <li>User Id: {p.user_id}</li>}</For>
+            </ul>
+        </div>
+        <div>
+            <p>Peers:</p>
+            <ul>
+                <For each={peers()} fallback={<li>No peers.</li>}>{(p) => <li>Peer "{p.userDeviceId}", clk {p.clk.toString()}</li>}</For>
             </ul>
         </div>
     </div>;
