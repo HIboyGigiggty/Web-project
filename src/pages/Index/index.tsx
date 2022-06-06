2489cf808d50e2729bb86e52cd06429a2786370d
Liei <ww15977627289@gmail.com>
2022-06-06 19:44:24 +0800

    src/pages/lndex : add a chip to the room name

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -9,78 +9,86 @@ import AddIcon from "@suid/icons-material/Add";
 import Card from "@suid/material/Card";
 import List from "@suid/material/List";
 import Avatar from "@suid/material/Avatar";
-{/*-----------------------------------------*/}
+{/*-----------------------------------------*/ }
 import CardActions from "@suid/material/CardActions";
 import Modal from "@suid/material/Modal";
 import ListItem from "@suid/material/ListItem";
-{/*-----------------------------------------*/}
+{/*-----------------------------------------*/ }
 import { Navigate, useNavigate } from "solid-app-router";
-import { Component, For, Match, Show, Switch,createResource,createSignal } from "solid-js";
+import { Component, For, Match, Show, Switch, createResource, createSignal } from "solid-js";
 import { createSupabase, createSupabaseAuth } from "solid-supabase";
 import BroadClient from "../../helpers/BroadClient";
 import CardContent from "@suid/material/CardContent";
 import Popover from "@suid/material/Popover";
 import Divider from "@suid/material/Divider";
+import Chip from "@suid/material/Chip";
+import Stack from "@suid/material/Stack";
+import FaceIcon from '@suid/icons-material/Face';
 
+const getWwidth = async () => {
+    let WindowWidth = window.innerWidth;
+    let mediumWidth = WindowWidth / 2;
+    let finishWidth = mediumWidth - 100;
+    return (finishWidth);
 
-
+}
 const useBroadClient = () => {
     const supabase = createSupabase();
 
     return new BroadClient(supabase);
 };
 
-const UserAvatar :Component=() =>{//头像组件
+const UserAvatar: Component = () => {//头像组件
     const auth = createSupabaseAuth();
     const user = auth.user();
-    let buttomRef : HTMLButtonElement;
+    let buttomRef: HTMLButtonElement;
     console.log(user);
-    const [datailPopoverOpen,setdatailPopoverOpen] = createSignal<boolean>(false);
-    return(
+    const [datailPopoverOpen, setdatailPopoverOpen] = createSignal<boolean>(false);
+    return (
         <>
             <Button
                 variant="contained"
                 size="small"
-                onClick={() =>setdatailPopoverOpen(true)}
+                onClick={() => setdatailPopoverOpen(true)}
                 //@ts-expect-error :The value is assigned by SolidJS when it is used
                 ref={buttomRef}
 
                 disableElevation
                 disableRipple
                 sx={{
-                    padding:0,
-                    minWidth:"25px",
+                    padding: 0,
+                    minWidth: "25px",
                 }}
             >
                 <Switch fallback={<Avatar>?</Avatar>}>
                     <Match when={user?.user_metadata.avatar_url}>
-                        <Avatar src={user?.user_metadata.avatar_url}/>  
+                        <Avatar src={user?.user_metadata.avatar_url} />
                     </Match>
                 </Switch>
             </Button>
             <Popover
                 anchorOrigin={{
-                    vertical:"bottom",
-                    horizontal:"left"
+                    vertical: "bottom",
+                    horizontal: "left"
                 }}
                 open={datailPopoverOpen()}
-              
+
                 onClose={() => setdatailPopoverOpen(false)}
                 //@ts-expect-error :The value is assigned by SolidJS when it is used
                 anchorEl={buttomRef}
             >
                 <Card>
-                    <CardContent sx={{padding:0}}style="padding: 0;">
+                    <CardContent sx={{ padding: 0 }} style="padding: 0;">
                         <Typography sx={{
-                            padding:0
+                            padding: 0
                         }}>
                             <List sx={{
-                                padding:0,
+                                padding: 0,
                             }}>
                                 <ListItem sx={{
-                                    padding:0
+                                    padding: 0
                                 }}>
-                                用户ID:<br></br>
+                                    用户ID:<br></br>
                                     {user?.id}
                                 </ListItem>
                             </List>
@@ -107,7 +115,7 @@ const Index: Component = () => {
             navigate("/login");
         }
     };
-    const [rooms, {refetch}] = createResource(getAllRooms, {
+    const [rooms, { refetch }] = createResource(getAllRooms, {
         initialValue: []
     });
     const creating = async () => {
@@ -123,22 +131,24 @@ const Index: Component = () => {
     };
 
 
-    {/*-----------------------------------------*/}
+
+
+    {/*-----------------------------------------*/ }
     const [open, setOpen] = createSignal(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    {/*-----------------------------------------*/}
+    {/*-----------------------------------------*/ }
 
     return <Show when={auth.user()} fallback={<Navigate href="/login" />}>
         {/*---------------------App bar--------------------*/}
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    
+
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         画板
                     </Typography>
-                    <UserAvatar/>
+                    <UserAvatar />
                 </Toolbar>
             </AppBar>
         </Box>
@@ -154,7 +164,7 @@ const Index: Component = () => {
         }}>
             <div>
                 <Fab color="primary" aria-label="add" onClick={handleOpen}>
-                    <AddIcon/>
+                    <AddIcon />
                 </Fab>
                 <Modal
                     open={open()}
@@ -179,8 +189,8 @@ const Index: Component = () => {
                         }}>
                             <List disablePadding>
                                 <ListItem disablePadding>
-                                    <TextField sx={{width: "100%"}} label="RoomName" variant="standard"
-                                        onChange={(_, val:string) => setRoomName(val)}
+                                    <TextField sx={{ width: "100%" }} label="RoomName" variant="standard"
+                                        onChange={(_, val: string) => setRoomName(val)}
                                         helperText="Please input your Room name" value={roomName()}> </TextField>
                                 </ListItem>
                             </List>
@@ -189,7 +199,7 @@ const Index: Component = () => {
                             <Box sx={{
                                 width: "100%",
                                 padding: "5px",
-                            }}/>
+                            }} />
                             <Button sx={{
                                 width: "50%",
                                 padding: "5px",
@@ -205,29 +215,36 @@ const Index: Component = () => {
         {/*--------------------FAB---------------------*/}
 
         {/*--------------------List---------------------*/}
-        <Box sx={{ml: "50%", transform: "translate(-50%, 0)", padding:0,marginTop:"60px"}}>
-            <Card sx={{minWidth:600,ml:"50%",transform:"translate(-50%,0)"}}>
-                <CardContent sx={{padding:0}} style="padding: 0;">
-                    <TextField sx={{width: "100%"}}  placeholder="房间ID"></TextField>
-                </CardContent>
-            </Card>
-        {/* </Box>
+        <div >
+            <Box sx={{ ml: "50%", transform: "translate(-50%, 0)", padding: 0, marginTop: "60px" }}>
+                <Card sx={{ minWidth: 600, ml: "50%", transform: "translate(-50%,0)" }}>
+                    <CardContent sx={{ padding: 0 }} style="padding: 0;">
+                        <TextField sx={{ width: "100%" }} placeholder="房间ID"></TextField>
+                    </CardContent>
+                </Card>
 
-        <Box sx={{ml: "50%", transform: "translate(-50%, 35%)", width: "fit-content", padding:0}}> */}
-            <Card sx={{minWidth:500,ml:"50%",transform:"translate(-50%,0)",marginTop: "30px"}}>
-                <CardContent sx={{padding:0}} style="padding: 0;">                
-                    <List sx={{padding:0}}>
-                        <For each={rooms()} fallback={<List>No rooms here.</List>}>
-                            {
-                                (item) => {
-                                    return <List> 房间名字 :"{item.name}"<Divider></Divider></List>;
+                <Card sx={{ minWidth: 500, ml: "50%", transform: "translate(-50%,0)", marginTop: "30px" }}>
+                    <CardContent sx={{ padding: 0 }} style="padding: 0;">
+                        <List sx={{ padding: 0 }}>
+                            <For each={rooms()} fallback={<List>No rooms here.</List>}>
+                                {
+                                    (item) => {
+                                        return <>
+                                            <ListItem >
+                                                房间名字 :"{item.name}"
+                                                <div className="Cchip"><Chip icon={<FaceIcon />} label="...房主信息..." sx={{ ml: "200px" }}></Chip></div>
+                                            </ListItem>
+                                            <Divider></Divider>
+                                        </>
+                                    }
                                 }
-                            }
-                        </For>
-                    </List>                   
-                </CardContent>
-            </Card>
-        </Box>
+                            </For>
+                        </List>
+                    </CardContent>
+                </Card>
+            </Box>
+        </div>
+
         {/*------------------List-----------------------*/}
 
     </Show>;
