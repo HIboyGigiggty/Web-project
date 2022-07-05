5a1c1625f1fa87717072c7b620b570bdf425656a
thisLight <l1589002388@gmail.com>
2022-07-06 00:54:48 +0800

    vite: add env var HOT_MODULE_REPLACEMENT

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,25 @@ import solidStyledJSXPlugin from "solid-styled-jsx/babel";
 import {visualizer} from "rollup-plugin-visualizer";
 import eslint from "vite-plugin-eslint";
 
+const getHMR = (): boolean | undefined => {
+    const hmr = process.env.HOT_MODULE_REPLACEMENT;
+    if (typeof hmr !== "undefined") {
+        if (hmr === "true") {
+            return true;
+        } else if (hmr === "false") {
+            return false;
+        } else {
+            throw new Error("env variable HOT_MODULE_REPLACEMENT should be true, false or undefined");
+        }
+    } else {
+        return undefined;
+    }
+};
+
 export default defineConfig(({mode}) => ({
     server: {
         port: 8080,
+        hmr: getHMR(),
     },
     plugins: [
         ...undestructurePlugin("ts"),
