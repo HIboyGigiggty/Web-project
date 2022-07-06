f20727c4a65d0f21df6f097c975f0e6f028fd7a8
thisLight <l1589002388@gmail.com>
2022-07-06 19:58:33 +0800

    vite: add runtime option STABLE_CHUNK_NAMES

--- docs/runtime_options.md
+++ docs/runtime_options.md
@@ -22,6 +22,15 @@ It could be undefined, `true` or `false`. This option is only taking effect on t
 - `false` will disable hot module replacement.
 - The default behavior will be used if it's not defined.
 
+### `STABLE_CHUNK_NAMES`
+It could be undefined, `true` or `false`. `true` or `false` means enable or disable this behavior, undefined means using default (disbaled). This option only take effect to rollup, used by `yarn build` (`vite build`) and `yarn watch` (`vite build -w`).
+
+Configure vite to produce stable chunk names. The packages in node_modules will be produced as `assets/<package name>.vendor<ext>`, source files will be produced as `assets/<relative path to the current working directory><ext>` with replacement of:
+
+- "." to "__"
+
+Default behavior is just better in most situation.
+
 ## Adding & Refering Runtime Options
 
 Application code should not directly access environment variables exposed by Vite. Environment variables should be organised in "src/configs" by their topics, and used by the rest of code.
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,75 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import {undestructurePlugin} from "babel-plugin-solid-undestructure";
 import solidStyledJSXPlugin from "solid-styled-jsx/babel";
 import {visualizer} from "rollup-plugin-visualizer";
 import eslint from "vite-plugin-eslint";
+import path from "path";
 
-const getHMR = (): boolean | undefined => {
-    const hmr = process.env.HOT_MODULE_REPLACEMENT;
-    if (typeof hmr !== "undefined") {
-        if (hmr === "true") {
+const getEnvBool = (name: string): boolean | undefined => {
+    const value = process.env[name];
+    if (typeof value !== "undefined") {
+        if (value === "true") {
             return true;
-        } else if (hmr === "false") {
+        } else if (value === "false") {
             return false;
         } else {
-            throw new Error("env variable HOT_MODULE_REPLACEMENT should be true, false or undefined");
+            throw new Error(`env variable ${name} should be true, false or undefined`);
         }
     } else {
         return undefined;
+    }
+};
+
+const getHMR = (): boolean | undefined => {
+    return getEnvBool("HOT_MODULE_REPLACEMENT");
+};
+
+const extractModChunkNameFromRelativePath = (relPath: string[]) => {
+    if (relPath[0].startsWith("@")) {
+        return relPath.slice(0, 2).join("_");
+    } else {
+        return relPath[0];
+    }
+};
+
+const manualChunksStable = (id: string): string | undefined => {
+    const name = id.split(path.sep);
+    if (name.includes("node_modules")) {
+        const nodeModulesI = name.findIndex((value) => value === "node_modules");
+        const nodeModulesPath = path.join(...name.slice(0, nodeModulesI+1));
+        const chunkPathRel = path.relative(nodeModulesPath, id).split(path.sep);
+        const chunkName = extractModChunkNameFromRelativePath(chunkPathRel.filter(value => value !== ".." && value !== "node_modules"));
+        return `${chunkName}.vendor`;
+    } else {
+        return path.relative(process.cwd(), id).replace(".", "__");
+    }
+};
+
+const getManualChunks = () => {
+    const stableChunkNames = getEnvBool("STABLE_CHUNK_NAMES");
+    if (typeof stableChunkNames === "boolean" && stableChunkNames) {
+        return manualChunksStable;
+    } else {
+        return undefined;
+    }
+};
+
+const getChunkFileNames = () => {
+    const stableChunkNames = getEnvBool("STABLE_CHUNK_NAMES");
+    if (typeof stableChunkNames === "boolean" && stableChunkNames) {
+        return "assets/[name].js";
+    } else {
+        return "assets/[name].[hash].js";
+    }
+};
+
+const getAssertFileNames = () => {
+    const stableChunkNames = getEnvBool("STABLE_CHUNK_NAMES");
+    if (typeof stableChunkNames === "boolean" && stableChunkNames) {
+        return "assets/[name][extname]";
+    } else {
+        return "assets/[name].[hash][extname]";
     }
 };
 
@@ -50,6 +103,13 @@ export default defineConfig(({mode}) => ({
         target: "modules",
         polyfillModulePreload: true,
         sourcemap: true,
+        rollupOptions: {
+            output: {
+                manualChunks: getManualChunks(),
+                chunkFileNames: getChunkFileNames(),
+                assetFileNames: getAssertFileNames(),
+            }
+        },
     },
     clearScreen: false,
 }));
