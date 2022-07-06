860763114ed665b7f88d743ddf7b9bf89e951132
thisLight <l1589002388@gmail.com>
2022-07-06 16:20:27 +0800

    env.d.ts: remove VITE_HOT_MODULE_REPLACEMENT

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_HOT_MODULE_REPLACEMENT?: boolean,
     readonly VITE_SUPABASE_API_URL: string,
     readonly VITE_SUPABASE_API_KEY_PUBLIC: string,
   }
